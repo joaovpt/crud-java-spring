1bc8738c60a4d78187a375b8e7d73c6eb04f83a6
joaovvpp <joaotassi18@gmail.com>
2024-09-10 16:19:09 -0300

    feat: add DELETE and PUT

--- src/main/java/com/example/crud/repositories/ProductRepository.java
+++ src/main/java/com/example/crud/repositories/ProductRepository.java
@@ -3,5 +3,8 @@ package com.example.crud.repositories;
 import com.example.crud.domain.product.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, String> {
+    List<Product> findAllByActiveTrue();
 }
