96f195a670224f6d81f4322245a478aad952b134
joaovvpp <joaotassi18@gmail.com>
2024-09-10 15:50:57 -0300

    feat: create RequestProductDTO

---
+++ src/main/java/com/example/crud/dtos/RequestProductDTO.java
@@ -0,0 +1,12 @@
+package com.example.crud.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record RequestProductDTO(
+        String id,
+        @NotBlank
+        String name,
+        @NotNull
+        Integer price_in_cents) {
+}
