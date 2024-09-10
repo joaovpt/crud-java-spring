4b56b656012e36f1e0b40b274b8af6417732e18d
joaovvpp <joaotassi18@gmail.com>
2024-09-10 15:50:26 -0300

    feat: create Product

---
+++ src/main/java/com/example/crud/domain/product/Product.java
@@ -0,0 +1,28 @@
+package com.example.crud.domain.product;
+
+import com.example.crud.dtos.RequestProductDTO;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name = "product")
+@Entity(name = "product")
+@EqualsAndHashCode(of = "id")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String name;
+    private Integer price_in_cents;
+
+    public Product(RequestProductDTO requestProductDTO){
+        this.name = requestProductDTO.name();
+        this.price_in_cents = requestProductDTO.price_in_cents();
+    }
+
+}
+
